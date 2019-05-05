6a0f52efaf9a23c3c819c122751032c0d22ef4e3
OneFragrance <beekwar@gmail.com>
2019-05-05 23:13:42 +0300

    [master] Added bson and pymongo libs

--- TrackEverything/TrackEverything/__init__.py
+++ TrackEverything/TrackEverything/__init__.py
@@ -2,7 +2,18 @@
 The flask application package.
 """
 
-from flask import Flask
+from flask import Flask, render_template,request,redirect,url_for
+from bson import ObjectId
+from pymongo import MongoClient
+import os
+
 app = Flask(__name__)
 
+title = "TODO sample application with Flask and MongoDB"
+heading = "TODO Reminder with Flask and MongoDB"
+client = MongoClient("mongodb://127.0.0.1:27017")
+db = client.mymongodb
+todos = db.todo
+
 import TrackEverything.views
+
