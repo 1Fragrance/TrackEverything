54269dd3ae520e7f3bc402eaa2cf746258b5f8b7
OneFragrance <beekwar@gmail.com>
2019-05-06 23:41:51 +0300

    [master] Added empty repositories

--- TrackEverything/TrackEverything/__init__.py
+++ TrackEverything/TrackEverything/__init__.py
@@ -6,11 +6,11 @@ from TrackEverything.models import db, Project, Task, Employee
 
 app = Flask(__name__)
 
-title = "TODO sample application with Flask and MongoDB"
-heading = "TODO Reminder with Flask and MongoDB"
-client = MongoClient("mongodb://127.0.0.1:27017")
-db = client.mymongodb
-todos = db.todo
+## include db name in URI; _HOST entry overwrites all others
+app.config['MONGODB_HOST'] = 'mongodb://localhost:27017/sivji-sandbox'
+app.debug = True
+
+db.init_app(app)
 
 import TrackEverything.views
 
---
+++ TrackEverything/TrackEverything/repositories/task_repository.py
@@ -0,0 +1,1 @@
+from TrackEverything.models import Task
