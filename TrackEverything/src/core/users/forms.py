f3a36978bbeddc2b6cb318a482f4c8d3979c1be3
Gintoki <beekwar@gmail.com>
2019-05-16 18:03:11 +0300

    [master] Working on user management

--- TrackEverything/src/core/users/forms.py
+++ TrackEverything/src/core/users/forms.py
@@ -1,14 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField
+from datetime import datetime, date, timedelta
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Optional, Length, ValidationError, Email
+from wtforms.fields.html5 import DateField
+from bson import ObjectId
+from src.models import STATUS_CHOICES, POSITION_CHOICES
+from src.models.user import User
+from src.models.task import Task
+from src.models.project import Project
 
 # TODO: Do this shit
-# User edit form
 class UserAssignForm(FlaskForm):
-    task = SelectField('Task')
+    username = StringField('Username', validators=[DataRequired(), Length(3, 255, 'Incorrect length')])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(2, 255, 'Incorrect length')])
+    username = StringField('Username', validators=[DataRequired(), Length(2, 255, 'Incorrect length')])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(2, 255, 'Incorrect length')])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(2, 255, 'Incorrect length')])
+    patronymic = StringField('Patronymic', validators=[Length(2, 255, 'Incorrect length')])
+    position = SelectField('Position', choices=POSITION_CHOICES, coerce=int, validators=[DataRequired()])
+    tasks = SelectField('Task')
     project = SelectField('Project')
+
     submit = SubmitField('Submit')
 
-    def __init__(self, *args, **kwargs):
-        super(UserAssignForm, self).__init__(*args, **kwargs)
-        self.task.choices = [(a.id, a.name) for a in Task.objects.all()]
-        self.project.choices = [(a.id, a.name) for a in Project.objects.all()]
+    def validate_email(self, field):
+        if User.objects(email=field.data):
+            raise ValidationError('Email is already in use.')
+
+    def validate_username(self, field):
+        if User.objects(username=field.data):
+            raise ValidationError('Username is already in use.')
--- TrackEverything/src/core/users/views.py
+++ TrackEverything/src/core/users/views.py
@@ -6,6 +6,7 @@ from src.models.task import Task
 from src.models.project import Project
 from src.models.user import User
 from ..views import is_admin
+from bson import ObjectId
 
 
 # Admin: Show all users
@@ -24,16 +25,14 @@ def list_users():
 @user.route('/users/assign/<string:id>', methods=['GET', 'POST'])
 @login_required
 def assign_user(id):
-    if not is_admin():
+    if not is_admin() or current_user.pk == ObjectId(id):
         abort(403)
     else:
         user = User.objects(pk=id).first()
 
-        # TODO: Think about this opportunity
-        if user.is_admin:
-            abort(403)
-
         form = UserAssignForm(obj=user)
+
+        # TODO: populate Project & Tasks 
         if request.method == 'POST' and form.validate_on_submit():
             user.project = form.department.data
             user.task = form.role.data
@@ -45,3 +44,19 @@ def assign_user(id):
         return render_template('core/users/user.html',
                                user=user, form=form,
                                title='Assign User')
+
+
+@user.route('/users/ban/<string:id>', methods=['GET', 'POST'])
+@login_required
+def ban_user(id):
+    if not is_admin() or current_user.pk == ObjectId(id):
+        abort(403)
+    else:
+        user = User.objects(pk=id).first()
+        user.status = 2
+        flash('User banned')
+
+        return redirect(url_for('user.list_users'))
+        # TODO: Check mb useless
+        return render_template(title="Ban user")
+        
